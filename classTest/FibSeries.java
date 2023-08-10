ce062c6dedca2e341137aa1347a10f1a2c6d980f
Gajendra Ingle <75525478+gajendra-ingle@users.noreply.github.com>
2023-08-10 18:52:16 +0530

    program added

--- classTest/FibSeries.java
+++ classTest/FibSeries.java
@@ -3,7 +3,18 @@ package classTest;
 public class FibSeries {
 	public static void main(String[] args) {
 		
-		
+		int num1 = 0;
+		int num2 = 1;
+		int num;
+		int size = 8;
+		System.out.print(num1 + " " + num2);
+
+		for (int i = 2; i <size; ++i) {
+			num = num1 + num2;
+			System.out.print(" " + num);
+			num1 = num2;
+			num2 = num;
+		}
 		
 		
 	}
