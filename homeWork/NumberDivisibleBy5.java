f228219e2caadd2bbaefb9d57c8dadba38f02b31
Gajendra Ingle <75525478+gajendra-ingle@users.noreply.github.com>
2023-08-12 14:33:36 +0530

    add some line of code

--- homeWork/NumberDivisibleBy5.java
+++ homeWork/NumberDivisibleBy5.java
@@ -1,5 +1,17 @@
 package homeWork;
 
 public class NumberDivisibleBy5 {
+        Scanner s = new Scanner(System.in);
+  
+        System.out.print("Enter any number:");
+        int n = s.nextInt();
+        if(n % 5 == 0)
+        {
+            System.out.println(n+" is divisible by 5");
+        }
+        else
+        {
+            System.out.println(n+" is not divisible by 5");
+        }
 
 }
